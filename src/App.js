e1f18cfda89a7d2d94310a5cb5899bd029014882
jasongreer12 <jasong@rae-corp.com>
2024-09-09 16:46:51 -0500

    Changes to app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
 import './App.css';
 import Logo from './classroomPhoto.jpg';
+import Form from 'react-bootstrap/Form';
 
 function App() {
+
+  const stateOptions = [
+    { value: "Choose", label: "Choose your state" },
+    { value: "OK", label: "Oklahoma" },
+  ];
+
+  const schoolOptions = [
+    { value: "Choose", label: "Choose your school" },
+    { value: "OSU", label: "Oklahoma State University" },
+  ];
+  const majorOptions = [
+    { value: "Choose", label: "Choose your major" },
+    { value: "CS", label: "Computer Science" },
+  ];
+  const jobOptions = [
+    { value: "Choose", label: "Choose desired job" },
+    { value: "CS", label: "Software Engineer" },
+  ];
+
   return (
     <div className="App">
       <img src={Logo} alt="Error loading image." className="logoID"></img>
-      <header className="overlay">
-        <div class="input-group mb-3 custom-width">
-          <div class="input-group-prepend">
-            <label class="input-group-text" for="inputGroupSelect01">Choose a school:</label>
+      <h1 id="appTitle">Student Career Helper</h1>
+      <div className="container-fluid" id="selectContainer" data-bs-theme="light">
+        <div className="row g-3">
+
+          <div className="col-md-12">
+            <label className="form-label" for="inputGroupSelect01" style={{ fontWeight: '700' }}>State:</label>
+            <Form.Select id="inputGroupSelect01" size="lg" className="form-select">
+              {stateOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </Form.Select>
           </div>
-          <select class="custom-select" id="inputGroupSelect01">
-            <option selected>Choose...</option>
-            <option value="okstate">Oklahoma State University</option>
-          </select>
+
+          <div className="col-md-12" >
+            <label className="form-label" for="inputGroupSelect02">School:</label>
+            <Form.Select id="inputGroupSelect02" size="lg" className="form-select">
+              {schoolOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </Form.Select>
+          </div>
+
+          <div className="col-md-12">
+            <label className="form-label" for="inputGroupSelect02">Choose a major:</label>
+            <Form.Select id="inputGroupSelect03" size="lg" className="form-select">
+              {majorOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </Form.Select>
+          </div>
+
+          <div className="col-md-12">
+            <label className="form-label" for="inputGroupSelect03" style={{ fontWeight: '700' }}>Job:</label>
+            <Form.Select id="inputGroupSelect04" size="lg" className="form-select">
+              {jobOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </Form.Select>
+          </div>
         </div>
-      </header>
+      </div>
     </div>
 
   );

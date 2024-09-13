33b4c540e13b2c912957be2d1934b181b0c41168
Keegneal <92490783+Keegneal@users.noreply.github.com>
2024-09-13 12:44:45 -0500

    Need to install env run: npm install dotenv

--- src/Backend/api.js
+++ src/Backend/api.js
@@ -1,1 +1,21 @@
 import axios from 'axios';
+
+const API_KEY = process.env.API_KEY;
+const BASE_URL = 'https://api.bls.gov/publicAPI/v2/timeseries/data/';
+
+const fetchSalaryData = async (occupationCode) => {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        seriesid: occupationCode,
+        startyear: '2018',
+        endyear: '2023',
+        registrationKey: API_KEY
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching salary data:", error);
+    throw error;
+  }
+};
